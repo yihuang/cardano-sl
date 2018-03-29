1d98c685da433f8e0f99af0ef84458e497834a41
Alexander Diemand <codieplusplus@apax.net>
2018-03-29 14:39:34 +0200

    CSL-2250 spec for CBR-97 - structured logging
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

---
+++ util/Pos/Util/Log.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Pos.Util.Log
+       ( logInfo
+       , slogInfo
+       ) where
+
+import           Universum
+
+import           Data.Aeson                  (ToJSON(..), encode)
+
+import qualified Katip                       as K
+
+
+{- same for other severities
+    https://github.com/Soostone/katip/blob/master/katip/src/Katip/Core.hs#L120
+-}
+-------------------------------------------------------------------------------
+-- | log a Text with severity = Info
+logInfo :: (K.KatipContext m, HasCallStack) => Text -> m ()
+logInfo msg = K.logItemM Nothing K.InfoS $ show msg
+
+-------------------------------------------------------------------------------
+-- | log a JSON structure with severity = Info
+slogInfo :: (ToJSON a, K.KatipContext m, HasCallStack) => a -> m ()
+slogInfo json = K.logItemM Nothing K.InfoS $ K.logStr $ encode json
+
+

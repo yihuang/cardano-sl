b2e134e489325b94f36fa3f8a1e638383dfbdbfe
Ben Ford <ben@perurbis.com>
2018-05-25 15:27:37 +0100

    Use a random value for the withdrawl amount

--- faucet/src/Cardano/Faucet.hs
+++ faucet/src/Cardano/Faucet.hs
@@ -17,7 +17,7 @@ import           System.Wlog (LoggerName (..), logError, logInfo, withSublogger)
 
 import           Cardano.Faucet.Types
 import           Cardano.Wallet.API.Response (WalletResponse (..))
-import           Cardano.Wallet.API.V1.Types (unV1)
+import           Cardano.Wallet.API.V1.Types (unV1, txAmount)
 import qualified Cardano.WalletClient as Client
 -- import           Client.Cardano.Wallet.Web.Run     (runEndpointClient)
 
@@ -26,7 +26,7 @@ type API = "withdraw" :> ReqBody '[JSON] WithDrawlRequest :> Post '[JSON] WithDr
 
 withdraw :: (MonadFaucet c m) => WithDrawlRequest -> m WithDrawlResult
 withdraw wd = withSublogger (LoggerName "withdraw") $ do
-    resp <- Client.withdraw (wd ^. wAddress) (wd ^. wAmount)
+    resp <- Client.withdraw (wd ^. wAddress)
     case resp of
         Left err -> do
             logError ("Error withdrawing " <> (wd ^. to show . packed)
@@ -35,9 +35,10 @@ withdraw wd = withSublogger (LoggerName "withdraw") $ do
             return $ WithdrawlError err
         Right wr -> do
             let txn = wrData wr
-            logInfo ((wd ^. to show . packed) <> " withdrawn. txn: "
-                                              <> (txn ^. to show . packed))
-            incWithDrawn (wd ^. wAmount . to unV1)
+                amount = unV1 $ txAmount txn
+            logInfo ((wd ^. to show . packed) <> " withdrew: "
+                                              <> (amount ^. to show . packed))
+            incWithDrawn amount
             return $ WithdrawlSuccess txn
 
 deposit :: (MonadFaucet c m) => DepositRequest -> m DepositResult
--- faucet/src/Cardano/WalletClient.hs
+++ faucet/src/Cardano/WalletClient.hs
@@ -12,6 +12,7 @@ module Cardano.WalletClient (
 import           Cardano.Faucet.Types
 import           Cardano.Wallet.API.V1.Types (Payment (..), PaymentDistribution (..), V1 (..))
 import           Cardano.Wallet.Client (Resp, Transaction, WalletClient (..), liftClient)
+import Control.Monad.IO.Class (liftIO)
 import           Control.Lens
 import           Crypto.Hash (Blake2b_256, Digest)
 import qualified Crypto.Hash as CryptoHash
@@ -19,15 +20,24 @@ import qualified Data.ByteArray as BA
 import           Data.ByteString (ByteString)
 import           Data.List.NonEmpty (NonEmpty (..))
 import Data.Text.Strict.Lens (utf8)
+import System.Random
+
 import           Pos.Core (Address (..), Coin (..))
 import           Pos.Crypto.Signing (PassPhrase)
 
 
+randomAmount :: (MonadFaucet c m) => m (V1 Coin)
+randomAmount = do
+    amt <- fromIntegral <$> view (feFaucetConfig . fcPaymentAmount . _Wrapped')
+    var <- view (feFaucetConfig . fcPaymentVariation . _Wrapped')
+    f <- liftIO $ randomRIO ((-1), 1)
+    return $ V1 $ Coin $ round (amt + (var * f))
 
-withdraw :: (MonadFaucet c m) => V1 Address -> V1 Coin -> Resp m Transaction
-withdraw addr coin = do
+withdraw :: (MonadFaucet c m) => V1 Address -> Resp m Transaction
+withdraw addr = do
     paymentSource <- view (fePaymentSource)
     spendingPassword <- view (feSpendingPassword . re utf8)
+    coin <- randomAmount
     client <- liftClient <$> view feWalletClient
     let paymentDist = (PaymentDistribution addr coin :| [])
         sp = Just $ V1 $ hashPwd spendingPassword
